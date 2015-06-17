45bcea99c306d747d6e76571a2be0be367baea87
Vincent Song <songvc@gmail.com>
2015-06-17 14:16:33 -0700

    portfolio section in progress

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,41 +2,84 @@
 
 angular.module('personalPortfolio')
   .controller('MainCtrl', function ($scope, $location, $anchorScroll) {
-  		$scope.myPortfolios = 
+  		$scope.menus = 
+  		[
+  			{
+  				"name":"About Me",
+  				"hash":"aboutme",
+  				"icon": "glyphicon-user"
+  			},
+ 			{
+  				"name":"Portfolio",
+  				"hash":"portfolio",
+  				"icon": "glyphicon-folder-open"
+  			},
+  			{
+  				"name":"Contact",
+  				"hash":"contact",
+  				"icon": "glyphicon-envelope"
+  			},
+  			{
+  				"name":"Blog",
+  				"hash":"blog",
+  				"icon": "glyphicon-pencil"
+  			}
+
+  		];
+  		$scope.works = 
   		[
 	  		{
 	  			"title" : "Waitstaff Calculator",
-	  			"description" : "I utilized Bootstrap and Angular to build an app that calculates the tax and tip of any restaurant bill.",
-	  			"image": "",
+	  			"description" : "Designed small business payroll application that tracks earnings of restaurant employees using Bootstrap",
+	  			"image": "../assets/images/soon.png",
 	  			"github" : "https://github.com/songvc/waitstaff",
 	  			"website": "http://www.google.com"		
 	  		},
 	  		{
 	  			"title" : "Country & Capitals",
-	  			"description" : "I utilized Bootstrap and Angular to build an app that searches and visualizes geodata",
-	  			"image": "",
+	  			"description" : "Created geo-data search application that allows users to interact with national city data using Geoname API",
+	  			"image": "../assets/images/soon.png",
 	  			"github" : "https://github.com/songvc/country",
 	  			"website": "http://www.google.com"		
 	  		},
 	  		{
-	  			"title" : "Calendar App",
-	  			"description" : "I utilized Bootstrap and Angular to build an app that calculates the tax and tip of any restaurant bill.",
-	  			"image": "",
+	  			"title" : "Calendar Directive",
+	  			"description" : "Designed calendar application UI using Angular directive and Bootstrap",
+	  			"image": "../assets/images/soon.png",
 	  			"github" : "https://github.com/songvc/angular-js-calendar-directive-challenge",
 	  			"website": "http://www.google.com"		
 	  		},
 	  		{
-	  			"title" : "Calendar App",
-	  			"description" : "I utilized Bootstrap and Angular to build an app that calculates the tax and tip of any restaurant bill.",
-	  			"image": "",
-	  			"github" : "https://github.com/songvc/angular-js-calendar-directive-challenge",
+	  			"title" : "Instagram Search",
+	  			"description" : "Created Instagram photo search application using Instagram API, Angular and Bootstrap",
+	  			"image": "../assets/images/soon.png",
+	  			"github" : "https://github.com/songvc/instagram",
 	  			"website": "http://www.google.com"		
-	  		}	
-	
+	  		},
+	  		{
+	  			"title" : "ngMadlibs",
+	  			"description" : "Utilized Bootstrap, Angular, Instagram API to build an mad-lib game ",
+	  			"image": "../assets/images/soon.png",
+	  			"github" : "https://github.com/songvc/ngMadLibs",
+	  			"website": "http://www.google.com"		
+	  		},
+	  		{
+	  			"title" : "Hot or Cold",
+	  			"description" : "Built Hot or Cold game application, designed its layout and style, and implemented its UI using JQuery",
+	  			"image": "../assets/images/soon.png",
+	  			"github" : "https://github.com/songvc/hot-or-cold-starter",
+	  			"website": "http://www.google.com"
+	  		},
 
+	  		{
+	  			"title" : "Street Fighter",
+	  			"description" : "Created 2D Street Fighter game application and designed its interactive features and animation using JQuery",
+	  			"image": "../assets/images/soon.png",
+	  			"github" : "https://github.com/songvc/jquery-streetfighter",
+	  			"website": "http://www.google.com"
+	  		}
 
-
-  		]
+  		];
 
   		$scope.gotoAnchor = function(hashtag){
   			console.log(hashtag);
