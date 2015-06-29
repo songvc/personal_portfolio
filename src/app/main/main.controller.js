e7a484ce1d66bf991b360395fabffaeb243c378f
Vincent Song <songvc@gmail.com>
2015-06-28 20:05:30 -0700

    making typography more responsive

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -24,7 +24,7 @@ angular.module('personalPortfolio')
   		];
 
   		$scope.skills=
-  		["HTML5","CSS3","JQuery","Angular","Bootstrap3","SASS","JavaScript","Gulp","Yeoman","Grunt","Protractor","Jasmine","Karma","Express","NodeJS","MongoDB","Firebase"];
+  		["HTML5","CSS3","JQuery","Angular","Bootstrap3","SASS","JavaScript","Gulp","Yeoman","Grunt","Protractor","Jasmine","Karma","Express","Node","MongoDB","Firebase","Nginx"];
   		
   		$scope.works = 
   		[
@@ -33,42 +33,48 @@ angular.module('personalPortfolio')
 	  			"description" : "Designed small business payroll application that tracks earnings of restaurant employees using Bootstrap",
 	  			"image": "../assets/images/waitstaff.png",
 	  			"github" : "https://github.com/songvc/wait-staff-refactor",
-	  			"website": "http://www.google.com"		
+	  			"website": "http://www.google.com",
+	  			"tooltip": "See Github"		
 	  		},
 	  		{
 	  			"title" : "Country & Capitals",
 	  			"description" : "Created geo-data search application that allows users to interact with national city data using Geoname API",
 	  			"image": "../assets/images/location.png",
 	  			"github" : "https://github.com/songvc/country",
-	  			"website": "http://www.google.com"		
+	  			"website": "http://www.google.com",
+	  			"tooltip": "See Github"		
 	  		},
 	  		{
 	  			"title" : "Calendar Directive",
 	  			"description" : "Designed calendar application UI using Angular directive and Bootstrap",
 	  			"image": "../assets/images/calendar.png",
 	  			"github" : "https://github.com/songvc/angular-js-calendar-directive-challenge",
-	  			"website": "http://www.google.com"		
+	  			"website": "http://www.google.com",
+	  			"tooltip": "See Github"		
 	  		},
 	  		{
 	  			"title" : "Instagram Search",
 	  			"description" : "Created Instagram photo search application using Instagram API, Angular and Bootstrap",
 	  			"image": "../assets/images/instagram.png",
 	  			"github" : "https://github.com/songvc/instagram",
-	  			"website": "http://www.google.com"		
+	  			"website": "http://www.google.com",
+	  			"tooltip": "See Github"		
 	  		},
 	  		{
 	  			"title" : "ngMadlibs",
 	  			"description" : "Utilized Bootstrap and Angular to build an mad-lib game ",
 	  			"image": "../assets/images/ngmadlib.png",
 	  			"github" : "https://github.com/songvc/ngMadLibs",
-	  			"website": "http://www.google.com"		
+	  			"website": "http://www.google.com",
+	  			"tooltip": "See Github"		
 	  		},
 	  		{
 	  			"title" : "Hot or Cold",
 	  			"description" : "Built Hot or Cold game application, designed its layout and style, and implemented its UI using JQuery",
 	  			"image": "../assets/images/hotorcold.png",
 	  			"github" : "https://github.com/songvc/hot-or-cold-starter",
-	  			"website": "http://www.google.com"
+	  			"website": "http://www.google.com",
+	  			"tooltip": "See Github"
 	  		},
 
 	  		{
@@ -76,7 +82,8 @@ angular.module('personalPortfolio')
 	  			"description" : "Created 2D Street Fighter game application and designed its interactive features and animation using JQuery",
 	  			"image": "../assets/images/streetfighter.png",
 	  			"github" : "https://github.com/songvc/jquery-streetfighter",
-	  			"website": "http://www.google.com"
+	  			"website": "http://www.google.com",
+	  			"tooltip": "See Github"
 	  		}
 
   		];
@@ -85,7 +92,6 @@ angular.module('personalPortfolio')
 	        $location.hash(hashtag);
 	        $anchorScroll();
   		}
-
 
 
   });
